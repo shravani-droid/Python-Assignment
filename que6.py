744ae9c00a78f54494adf7d33fa5a4870550857d
shravani-droid <shravanib317@gmail.com>
2026-06-15 09:38:46 +0530

    Add files via upload

---
+++ que6.py
@@ -0,0 +1,15 @@
+print("Enter first number: ")
+no1=int(input())
+
+print("Enter second number: ")
+no2=int(input())
+
+Add=no1+no2
+Sub=no1-no2
+Mul=no1*no2
+Div=no1/no2
+
+print("addition is:",Add)
+print("substraction is:",Sub)
+print("multiplication is:",Mul)
+print("division is:",Div)
