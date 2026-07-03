3804793d4a081d5d8de24e5b8b9ce6c908dad812
shravani-droid <shravanib317@gmail.com>
2026-07-03 22:22:06 +0530

    Implement binary conversion program
    Added a program to convert a number to its binary equivalent.

---
+++ Assignment-13/program4.py
@@ -0,0 +1,14 @@
+#program which accept number and prints binary equivalent
+
+def BinaryCheck(num):
+  print(bin(num))
+ 
+
+def main():
+ number = int(input("Enter The Number:"))
+
+ BinaryCheck(number)
+
+ 
+if __name__ == "__main__":
+  main()
